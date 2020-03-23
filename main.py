5128679d5e088097d6cfefa0991b897e863f752c
m4ever1 <50123559+m4ever1@users.noreply.github.com>
2020-03-23 15:30:34 +0000

    goal test finished, needs testing

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ class state_t:
 
         for shift in shifts:
             self.shifts[shift.nr] = shift
-    def addShift(self, shift):
-        self.shifts.append(shift)
 
 
 class Shift:
@@ -29,8 +27,8 @@ class Shift:
             return -1
         else:
             self.dictOfNurses[Nurse.ID] = Nurse
-            self.NrNurses = self.NrofNurses + 1
-            print("Added nurse" + Nurse.ID + "to" + self.time + "Shift in day" + self.weekDay.day + "\n")
+            self.NrNurses = self.NrNurses + 1
+            # print("Added nurse" + Nurse.ID + "to" + self.time + "Shift in day" + self.weekDay.day + "\n")
             return 1
 
 
@@ -74,7 +72,8 @@ class ScheduleP(search.Problem):
                     if nurse.ID not in shift.dictOfNurses:
                         if shift.prevshift is None:
                             actions.append((nurse, shift))
-                        elif shift.prevshift.dictOfNurses[nurse.ID] is not shift.prevshift.prevshift.dictOfNurses[nurse.ID]:
+                        elif shift.prevshift.dictOfNurses[nurse.ID] is not shift.prevshift.prevshift.dictOfNurses[
+                            nurse.ID]:
                             actions.append((nurse, shift))
 
     def result(self, state, action):
@@ -90,26 +89,14 @@ class ScheduleP(search.Problem):
         state to self.goal or checks for state in self.goal if it is a
         list, as specified in the constructor. Override this method if
         checking against a single self.goal is not enough."""
-
-        target_list = []
-        goal = False
-
         # If in all shifts the number of nurses is equal to the number of nurses 
-        # required then all shifts have been successfully atributed
+        # required then all shifts have been successfully attributed
+        goal = True
         for shift in state.shifts:
-            if shift.NrNurses == shift.nrNursesReq:
-                target_list.append(True)
-            else:
-                target_list.append(False)
-
-        for target in target_list:
-            if target:
-                goal = True
-            else:
+            if shift.NrNurses != shift.nrNursesReq:
                 goal = False
+                break
         return goal
-
-
 
     def path_cost(self, c, state1, action, state2):
         """Return the cost of a solution path that arrives at state2 from
@@ -117,25 +104,13 @@ class ScheduleP(search.Problem):
         is such that the path doesn't matter, this function will only look at
         state2.  If the path does matter, it will consider c and maybe state1
         and action. The default method costs 1 for every step in the path."""
-
+        cost = 0
         flag = False
 
-        for nurse in state2.nurses:
-            for i in range(len(nurse.shiftList)):
-                for j in range(i + 1, len(nurse.shiftList)):
-                    if nurse.shiftList[i].weekDay == nurse.shiftList[j].weekDay:
-                        if (nurse.shiftList[i].time == 'Morning' and nurse.shiftList[j].time == 'Afternoon') or \
-                        (nurse.shiftList[i].time == 'Afternoon' and nurse.shiftList[j].time == 'Night'):
-                            flag = True
+        if action[0].ID in state2.shifts[action[1]].prevshift.dictOfNurse.keys():
+            cost = 1
 
-        if flag:
-             return c = c + 2
-        else:
-            return c = c + 0
-
-
-
-        return c + 1
+        return c + cost
 
     def value(self, state):
         """For optimization problems, each state has a value. Hill-climbing
@@ -151,21 +126,24 @@ def BuildClass():
         nList.append(Nurse(i))
     for i in range(1, 8):
         if i == 1:
-            s1 = Shift(i+1, 9, "Morning", None)
-            s2 = Shift(i+2, 6, "Afternoon", s1)
-            s3 = Shift(i+3, 5, "Night", s2)
+            s1 = Shift(i + 1, 9, "Morning", None)
+            s2 = Shift(i + 2, 6, "Afternoon", s1)
+            s3 = Shift(i + 3, 5, "Night", s2)
         if i == 2 or 4 or 5:
-            s1 = Shift(i+1, 9, "Morning", s3)
-            s2 = Shift(i+2, 6, "Afternoon", s1)
-            s3 = Shift(i+3, 5, "Night", s2)
+            s1 = Shift(i + 1, 9, "Morning", s3)
+            s2 = Shift(i + 2, 6, "Afternoon", s1)
+            s3 = Shift(i + 3, 5, "Night", s2)
         if i == 3 or 6 or 7:
-            s1 = Shift(i+1, 6, "Morning", s3)
-            s2 = Shift(i+2, 6, "Afternoon", s1)
-            s2 = Shift(i+3, 5, "Night", s2)
+            s1 = Shift(i + 1, 6, "Morning", s3)
+            s2 = Shift(i + 2, 6, "Afternoon", s1)
+            s2 = Shift(i + 3, 5, "Night", s2)
         sList.append(s1)
         sList.append(s2)
         sList.append(s3)
     return nList, sList
 
+
 nlist, slist = BuildClass()
 Pb = ScheduleP(nlist, slist)  # INITIAL STATE
+
+node = search.astar_search(Pb, None)
